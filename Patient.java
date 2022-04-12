7fb8e3fef16ac8b1123c313955fd966b44da0156
Arne Malfait <arne.malfait@Ugent.be>
2022-04-12 17:41:31 +0200

    test

---
+++ Patient.java
@@ -0,0 +1,32 @@
+public class Patient {
+
+    // variables for patient class
+    int nr;
+    int patientType;
+    int scanType;
+    int callWeek;
+    int callDay;
+    double callTime;
+    int scanWeek = -1;
+    int scanDay = -1;
+    int slotNr = -1;
+    double appTime = -1;
+    double tardiness;
+    boolean isNoShow;
+    double scanTime = -1;
+    double duration;
+
+    // method to return the app waiting time
+
+    static void getAppWT(){
+
+    }
+
+    // method to return the scan waiting time
+    static void getScanWT(){
+
+    }
+
+
+
+}
